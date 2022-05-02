7dc4a4d95f29613a18bde4a9e8e885ef30297077
Nikolos-S <sarkozzy@mail.ru>
2022-05-02 16:29:12 +0700

    fix test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -7,10 +7,9 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getFixturePath = (filename) => join(__dirname, '..', '__fixtures__', filename);
 
-const expectedJson = readFileSync(getFixturePath('expectedJson.txt'), 'utf8');
-const pathFileJsonOne = getFixturePath('file1.json');
-const pathFileJsonTwo = getFixturePath('file2.json');
-
 test('Comparison of flat files (JSON)', () => {
+  const expectedJson = readFileSync(getFixturePath('expectedJson.txt'), 'utf8');
+  const pathFileJsonOne = getFixturePath('file1.json');
+  const pathFileJsonTwo = getFixturePath('file2.json');
   expect(gendiff(pathFileJsonOne, pathFileJsonTwo)).toBe(expectedJson);
 });
